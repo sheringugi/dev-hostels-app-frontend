7d19269bad4d56bd2fcc445f464ebf0f51fd040b
sheringugi <sheri.ngugi17@gmail.com>
2023-08-03 14:48:32 +0300

    Update dependencies

--- src/Hostel_listing.jsx
+++ src/Hostel_listing.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faBed } from "@fortawesome/free-solid-svg-icons";
 import FilterByLocation from "./FilterByLocation";
 import FilterByRoom from "./FilterByRoom";
 import "./Hostel_listing.css";
