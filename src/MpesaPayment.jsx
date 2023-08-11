b0db0475324439bc70f256193a2e3d8c13336724
PhoebeTracyChaoKazungu <kazungutracy711@gmail.com>
2023-08-11 15:37:51 +0300

    Update Mpesa

--- src/MpesaPayment.jsx
+++ src/MpesaPayment.jsx
@@ -1,47 +1,118 @@
-// MpesaPayment.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const MpesaPayment = ({ totalPrice, onPaymentSuccess }) => {
+const MpesaPayment = ({ totalPrice }) => {
+  const [countryCode, setCountryCode] = useState('254');
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [queryResult, setQueryResult] = useState(null);
+  const [paymentStatus, setPaymentStatus] = useState('');
+  const [queryStatus, setQueryStatus] = useState('');
+
+  const [receipt, setReceipt] = useState([]);
+  const [isPaymentSuccessful, setIsPaymentSuccessful] = useState(false);
+
+  const url = "https://4fc2-197-139-44-10.ngrok-free.app";
 
   const handlePay = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:3000/stkpush', {
+      const fullPhoneNumber = `${countryCode}${phoneNumber}`;
+  
+      const response = await fetch(`${url}/stkpush`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          phoneNumber,
+          phoneNumber: parseInt(fullPhoneNumber),
           amount: totalPrice.toString(),
         }),
       });
-
+  
       if (response.ok) {
-        onPaymentSuccess(); // Notify parent component of successful payment
+        setIsPaymentSuccessful(true);
+        const data = await response.json();
+        setReceipt(data);
+        setPaymentStatus(data.ResponseDescription);
       } else {
-        console.error('Payment failed');
+        setPaymentStatus('Payment failed');
       }
     } catch (error) {
       console.error('Error:', error);
     }
   };
+  
+  const checkPayment = async () => {
+    try {
+      if (receipt.length === 0 || !receipt[1]["CheckoutRequestID"]) {
+        setQueryStatus('No receipt available');
+        return;
+      }
+  
+      const response = await fetch(`${url}/stkquery`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          checkoutRequestID: receipt[1]["CheckoutRequestID"],
+        }),
+      });
+  
+      if (response.ok) {
+        const result = await response.json();
+        setQueryResult(result);
+        setQueryStatus(result[1]["ResultCode"] === 0 ? 'Payment confirmed' : 'Payment was unsuccessful');
+      } else {
+        setQueryStatus('Query failed');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  
+  useEffect(() => {
+    if (isPaymentSuccessful) {
+      const timer = setTimeout(() => {
+        setIsPaymentSuccessful(false);
+        setPaymentStatus('');
+      }, 5000); // Clear the success message after 5 seconds
+
+      return () => clearTimeout(timer);
+    }
+  }, [isPaymentSuccessful]);
 
   return (
     <div>
-     <form onSubmit={handlePay}>
       <h2>M-Pesa Payment</h2>
       <p>Total Amount: ${totalPrice}</p>
       <label>
         Phone Number:
-        <input
-          type="text"
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
-        />
+        <div>
+          <select
+            value={countryCode}
+            onChange={(e) => setCountryCode(e.target.value)}
+          >
+            <option value="254">+254 (Kenya)</option>
+            <option value="255">+255 (Tanzania)</option>
+            <option value="256">+256 (Uganda)</option>
+            {/* Add more country codes and names as needed */}
+          </select>
+          <input
+            type="text"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+        </div>
       </label>
-      <button type="submit">Pay</button>
-    </form>
+      <button onClick={handlePay}>Pay</button>
+      <p>{isPaymentSuccessful ? 'Payment successful' : paymentStatus}</p>
+      {receipt.length > 0 && (
+        <div>
+          {/* <h3>Receipt:</h3> */}
+          <p>{receipt[0]?.ResponseDescription}</p> {/* Display only the ResponseDescription */}
+          <button onClick={checkPayment}>Check Payment Status</button>
+          <p>{queryStatus}</p>
+        </div>
+      )}
     </div>
   );
 };
--- src/MpesaPaymentPage.jsx
+++ src/MpesaPaymentPage.jsx
@@ -1,55 +1,9 @@
-// import React from 'react';
-// import { useParams } from 'react-router-dom';
-// import PropTypes from 'prop-types';
-// import MpesaPayment from './MpesaPayment';
-// import ProtectedNavBar from "./ProtectedNavBar"
-
-// const MpesaPaymentPage = () => {
-//   const { totalPrice } = useParams();
-
-//   if (typeof totalPrice === 'undefined') {
-//     // Handle the case where totalPrice is not provided
-//     return <div>Loading...</div>;
-//   }
-
-//   return (
-//     <>
-//     <ProtectedNavBar />
-//     <div className="payment-page-container">
-//       <div className="image-container">
-//         {/* Your image */}
-//         <img
-//           className="apartment-image"
-//           src="https://images.pexels.com/photos/9170356/pexels-photo-9170356.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-//           alt="apartment"
-//         />
-
-//         <div className="form-overlay">
-//           <div className="centered-form">
-//             <MpesaPayment totalPrice={parseInt(totalPrice)} />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-      
-//     </>
- 
-//   );
-// };
-
-// // Define PropTypes for the component
-// MpesaPaymentPage.propTypes = {
-//   totalPrice: PropTypes.number.isRequired,
-// };
-
-// export default MpesaPaymentPage;
-// MpesaPaymentPage.js
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import MpesaPayment from './MpesaPayment';
-import './MpesaPaymentPage.css'; 
-import ProtectedNavBar from "./ProtectedNavBar"// Import your custom CSS file
+import './MpesaPaymentPage.css';
+import ProtectedNavBar from './ProtectedNavBar'; // Import your custom CSS file
 
 const MpesaPaymentPage = () => {
   const { totalPrice } = useParams();
@@ -61,23 +15,23 @@ const MpesaPaymentPage = () => {
 
   return (
     <>
-    <ProtectedNavBar />
-    <div className="payment-page-container">
-      <div className="image-container">
-        {/* Your image */}
-        <img
-          className="apartment-image"
-          src="https://images.pexels.com/photos/9170356/pexels-photo-9170356.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="apartment"
-        />
+      <ProtectedNavBar />
+      <div className="payment-page-container">
+        <div className="image-container">
+          {/* Your image */}
+          <img
+            className="apartment-image"
+            src="https://images.pexels.com/photos/9170356/pexels-photo-9170356.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="apartment"
+          />
 
-        <div className="form-overlay">
-          <div className="centered-form">
-            <MpesaPayment totalPrice={parseInt(totalPrice)} />
+          <div className="form-overlay">
+            <div className="centered-form">
+              <MpesaPayment totalPrice={parseInt(totalPrice)} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
